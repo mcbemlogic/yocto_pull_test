9823d8a31c05d9f32f5a99aabf71aeca8b00c4ce
mcbemlogic <127757646+mcbemlogic@users.noreply.github.com>
2023-03-30 15:45:30 +0200

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -3,5 +3,9 @@
 int main()
 {
     printf("Hello from Github, attempt with packagegroups\n");
+    
+    // Add some obvious lines so I can see it in the systemd boot sequence
+    printf("*********************\n\n\n\n\n\n\n\n\n\n********************");
+    
     return 0;
 }
